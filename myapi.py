66a447e6e989915f48b76f478c906965f5fda884
Dayana <dayanadiasgarcia@gmail.com>
2022-10-23 00:08:03 -0300

    feat: add game 'adivinha o número'

--- myapi.py
+++ myapi.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from random import randint
+
 app = Flask(__name__)
 
-@app.route('/')
-def ola_mundo():
-	return render_template("index.html")
+@app.route('/', methods=["GET", "POST"])
+def index():
+  var = "Game: adivinhe o número correto"
+
+  if request.method == "GET":
+    return render_template("index.html", var = var)
+  else:
+    numero = randint(1, 10)
+    palpite = int(request.form.get("name"))
+    if numero == palpite:
+      return '<h1>Resultado: Você ganhou!!!!</h1>'
+    else:
+      return '<h1>Resultado: Você perdeu!!!!</h1>'
+
 
 @app.route('/<string:nome>')
 def error(nome):
